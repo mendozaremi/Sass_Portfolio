9a6af59af1fe57a509ef2a3700202e90f839640f
Remi Mendoza <mendozaremi@gmail.com>
2020-04-27 04:25:09 -0400

    hamburger button

---
+++ dist/js/main.js
@@ -0,0 +1,25 @@
+// Select DOM items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItem = document.querySelectorAll('.nav-item')
+
+
+let showmenu = false;
+
+menuBtn.addEventListener('click', toggleMenu)
+
+
+function toggleMenu() {
+  if(!showmenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+  } else {
+    // Set menu state
+    showMenu = false
+  }
+}
